c33c90c2a488f1bf7322ad940922059d1c19dbb1
Ellinor Henriksson <eh224kr@student.lnu.se>
2022-12-06 12:11:05 +0100

    GREEN Implement Console constructor

--- app/src/main/java/diceGame/view/Console.java
+++ app/src/main/java/diceGame/view/Console.java
@@ -5,9 +5,11 @@ import java.util.Scanner;
 
 public class Console {
   private Scanner scan;
+  private PrintStream print;
 
   public Console() {
     scan = new Scanner(System.in, "utf-8");
+    print = new PrintStream(System.out);
   }
   
   public void printMenu() {
