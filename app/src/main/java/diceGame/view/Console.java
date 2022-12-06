31de5d95694a61a21903258f8443e0063560647e
Ellinor Henriksson <eh224kr@student.lnu.se>
2022-12-06 11:28:28 +0100

    GREEN Implement Console constructor

--- app/src/main/java/diceGame/view/Console.java
+++ app/src/main/java/diceGame/view/Console.java
@@ -1,6 +1,14 @@
 package diceGame.view;
 
+import java.util.Scanner;
+
 public class Console {
+  private Scanner scan;
+
+  public Console() {
+    scan = new Scanner(System.in, "utf-8");
+  }
+  
   public void printMenu() {
     
   }
