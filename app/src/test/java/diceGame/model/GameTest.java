4ac9481d850b9532cd60e8770a1ace045d46b553
Ellinor Henriksson <eh224kr@student.lnu.se>
2022-12-07 12:44:33 +0100

    RED Test that Game.newGame returns computer if scores are same

--- app/src/test/java/diceGame/model/GameTest.java
+++ app/src/test/java/diceGame/model/GameTest.java
@@ -62,4 +62,19 @@ public class GameTest {
       assertSame(expected, actual);
     }
   }
+
+  @Test void newGameShouldReturnComputerIfScoresAreSame() {
+    try (MockedConstruction<Player> mock = mockConstruction(Player.class)) {
+      Game sut = new Game();
+      Player mockedPlayer = mock.constructed().get(0);
+      Player mockedComputer = mock.constructed().get(1);
+      when(mockedPlayer.getScore()).thenReturn(2);
+      when(mockedComputer.getScore()).thenReturn(2);
+
+      Player expected = mockedComputer;
+      Player actual = sut.newGame();
+      
+      assertSame(expected, actual);
+    }
+  }
 }
