6bac1d3ba4540779e872d607089d5b6c8090edc6
Ellinor Henriksson <eh224kr@student.lnu.se>
2022-12-19 17:18:34 +0100

    GREEN Implement model.Player constructor to check for non-letter characters

--- app/src/main/java/diceGame/model/Player.java
+++ app/src/main/java/diceGame/model/Player.java
@@ -6,14 +6,21 @@ public class Player {
 
   private int minLength = 3;
   private int maxLength = 30;
+  private String allowedChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
   public Player(String name) {
     setName(name);
   }
 
-  private void setName(String name) {
+  public void setName(String name) {
     if (name.length() < minLength || name.length() > maxLength) {
       throw new IllegalArgumentException("Name must be 3-30 characters long.");
+    }
+
+    for (char c : name.toCharArray()) {
+      if (allowedChars.indexOf(c) == -1) {
+        throw new IllegalArgumentException("Name must only contain English letters.");
+      }
     }
   
     this.name = name;
